518285468645f4b706ccfd0a80a92e89d2563e7b
Javier Salazar Loyola <jsalazarloyola@gmail.com>
2016-09-26 21:57:08 -0300

    Add basic inputs to mainwindow

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -26,7 +26,7 @@ class MainApp(Gtk.Window, Render):
         Render.__init__(self,n, front, back)
 
         # Title and window stuff
-        self.set_border_width(3)
+        self.set_border_width(5)
         self.set_title("Fractures")
         self.resize(width,height)
         self.set_position(Gtk.WindowPosition.CENTER)
@@ -35,11 +35,16 @@ class MainApp(Gtk.Window, Render):
         self.connect("destroy", Gtk.main_quit)
 
         # Call the input creations
-        self.__createInputs()
-        
+        self.inputGrid = self.__createInputs()
         # The draw area for the cairo canvas
         self.darea = Gtk.DrawingArea()
-        #self.add(self.darea)
+
+        # Grid for holding the drawing area (right) and the inputs
+        self.grid = Gtk.Grid()
+        self.grid.add(self.inputGrid)
+        self.grid.add(self.darea)
+        
+        self.add(self.grid)
 
         # Show everything on the window
         self.show_all()
@@ -50,31 +55,33 @@ class MainApp(Gtk.Window, Render):
     def start(self):
         Gtk.main()
 
+    # Method for setting all the inputs in the window
+    # TODO: -Move the box to a grid
+    #       -Add labels to everything
+    #       -Figure a way to have a default value for the ComboBox
     def __createInputs(self):
-        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
                            spacing=6)
-
-        self.add(self.vbox)
 
         # Speed of fracture
         self.speedEntry = Gtk.Entry()
         self.speedEntry.set_text("Speed of fractures")
-        self.vbox.pack_start(self.speedEntry, True, False, 0)
+        vbox.pack_start(self.speedEntry, True, False, 0)
 
         # Size of... something.
         self.sizeEntry = Gtk.Entry()
         self.sizeEntry.set_text("Size")
-        self.vbox.pack_start(self.sizeEntry, True, False, 0)
+        vbox.pack_start(self.sizeEntry, True, False, 0)
 
         # Distance of source?
         self.distanceEntry = Gtk.Entry()
         self.distanceEntry.set_text("Distance")
-        self.vbox.pack_start(self.distanceEntry, True, False, 0)
+        vbox.pack_start(self.distanceEntry, True, False, 0)
         
         # Number of sources
         self.sourceNumber = Gtk.Entry()
         self.sourceNumber.set_text("Sources number")
-        self.vbox.pack_start(self.sourceNumber, True, False, 0)
+        vbox.pack_start(self.sourceNumber, True, False, 0)
 
         domainStore = Gtk.ListStore(str, str)
         domainStore.append(['circ', 'Circle'])
@@ -87,13 +94,22 @@ class MainApp(Gtk.Window, Render):
         self.domain.pack_start(renderer, True)
         self.domain.add_attribute(renderer, "text", 1)
         
-        self.vbox.pack_start(self.domain, False, False, True)
+        vbox.pack_start(self.domain, False, False, True)
 
         # Start algorithm button
         startButton = Gtk.Button.new_with_label("Start")
         startButton.connect("clicked", self.startAlgorithm)
-        
+        self.runSpinner = Gtk.Spinner()
+        #table = Gtk.Grid()
+        #table.attach(startButton, 1, 0, 1, 1)
+        #table.attach(self.runSpinner, 1, 2, 2, 3)
+        # The button stays at the end
+        vbox.pack_end(self.runSpinner, False, False, False)
+        vbox.pack_end(startButton, False, False, False)
 
+        # End of function
+        return vbox
+        
     # What to do when the selection changes in the list of domain options
     def onSelectionChange(self, combo):
         treeiter = combo.get_active_iter()
@@ -102,5 +118,8 @@ class MainApp(Gtk.Window, Render):
             print("Selected:",model[treeiter][0])
 
     # This function will call the algorithm
-    def startAlgorithm(self):
+    def startAlgorithm(self, button):
+        # Remember in the future to stop spinning
+        self.runSpinner.start()
         print("At this point, the algorithm will be executed.")
+        
